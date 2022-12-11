0ef3a834bf6ae11393f3e806c57d1f1fcb28848a
Edu25664 <eduardo.oliveiraramos12@hotmail.com>
2022-12-11 11:36:13 -0300

    primary entity

--- backend/src/main/java/com/devsuperior/dscatalog/resource/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resource/CategoryResource.java
@@ -1,25 +1,26 @@
 package com.devsuperior.dscatalog.resource;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dscatalog.Services.CategoryService;
 import com.devsuperior.dscatalog.entites.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-     
-    @GetMapping
-    public ResponseEntity<List<Category>>FindAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L,"BOOKS"));
-        list.add(new Category(1L,"eLETRONICS"));
 
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<Category>>findAll(){
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
