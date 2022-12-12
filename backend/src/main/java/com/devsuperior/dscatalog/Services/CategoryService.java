10daaf31dc469057ecc56bdacecc86ec34a28967
Edu25664 <eduardo.oliveiraramos12@hotmail.com>
2022-12-12 11:41:49 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/Services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/Services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog.Services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDto;
 import com.devsuperior.dscatalog.entites.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,12 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return repository.findAll();
-
+    public List<CategoryDto> findAll() {
+      List<Category> list =  repository.findAll();
+      List<CategoryDto> listDTO = new ArrayList<>();
+      for (Category cat : list){
+        listDTO.add(new CategoryDto(cat));
+      }
+       return listDTO;
     }
 }
